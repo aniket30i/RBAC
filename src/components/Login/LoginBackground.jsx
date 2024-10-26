3f1ffcbefe4d5f160f655c2d3c6c64eeeb676d6a
Aniket Saha <iamaniket.saha.09@protonmail.com>
2024-10-26 19:32:47 +0530

    designing the login page

--- src/components/Login/LoginBackground.jsx
+++ src/components/Login/LoginBackground.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const LoginBackground = () => {
   return (
     <div className="h-screen w-100 bg-black">
-      <p className="bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-9xl text-transparent">
-        Manage your account
-      </p>
+      <div className="flex h-full w-full items-center justify-center">
+        <p className="bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-9xl text-transparent">
+          Manage your account
+        </p>
+      </div>
     </div>
   );
 };
