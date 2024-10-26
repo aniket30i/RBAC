49f5bdc374e6c4a298f48e0e87ed5780ac4c2d54
Aniket Saha <iamaniket.saha.09@protonmail.com>
2024-10-26 19:45:55 +0530

    ...

--- src/components/Login/LoginBackground.jsx
+++ src/components/Login/LoginBackground.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const LoginBackground = () => {
   return (
-    <div className="h-screen w-100 bg-black">
+    <div className="h-screen w-100 bg-black relative">
       <div className="flex h-full w-full items-center justify-center">
-        <p className="bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-9xl text-transparent">
-          Manage your account
-        </p>
+        <h1 className="bg-gradient-to-r from-orange-600 to-stone-300 bg-clip-text text-[10rem] text-transparent">
+          Manage your Employees
+        </h1>
+      </div>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-red-300 p-4">
+        <div className="bg-red-500 p-4">
+          <p className="text-white text-2xl">Hello form</p>
+        </div>
       </div>
     </div>
   );
