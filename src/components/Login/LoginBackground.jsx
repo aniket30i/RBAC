1e8c9467def0c619d43fefc6abe51381c906ed74
Aniket Saha <iamaniket.saha.09@protonmail.com>
2024-10-27 03:08:12 +0530

    Made the login form as a component clearing the LoginBackground component

--- src/components/Login/LoginBackground.jsx
+++ src/components/Login/LoginBackground.jsx
@@ -1,45 +1,14 @@
-import React from "react";
-import CustomDropdown from "./Dropdown";
+import LoginForm from "./LoginForm";
 
 const LoginBackground = () => {
   return (
-    <div className="h-screen w-100 bg-black relative">
+    <div className="h-screen w-100 bg-zinc-950 relative">
       <div className="flex h-full w-full items-center justify-center">
         <h1 className="bg-gradient-to-r from-orange-600 to-stone-300 bg-clip-text text-[10rem] text-transparent text-center">
           Employee Management Simplified
         </h1>
       </div>
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-red-300 p-4">
-        <div className="bg-slate-500 p-4">
-          <div className="flex">
-            <div>PIC</div>
-            <div className="border-2 border-emarald-700 min-h-[500px] min-w-[300px]">
-              <div className="flex flex-col gap-8 pt-8">
-                <h4 className="text-white text-4xl">Welcome Back</h4>
-                <p>Login as Admin/User to continue.</p>
-              </div>
-
-              <form action="" className="flex flex-col gap-4 mt-12">
-                <CustomDropdown />
-                <input
-                  type="text"
-                  placeholder="Enter Username"
-                  className="fieldset"
-                />
-
-                <input
-                  type="password"
-                  placeholder="Enter Password"
-                  className="fieldset"
-                />
-              </form>
-              <button className="p-3 px-10 rounded-2xl bg-orange-500">
-                Login
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LoginForm />
     </div>
   );
 };
--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,7 +1,47 @@
-import React from "react";
-
+import FrontImage from "../../assets/loginside.jpg";
+import CustomDropdown from "./Dropdown";
 const LoginForm = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  p-6 bg-zinc-950/10 rounded-xl">
+        <div className="bg-zinc-900/70 p-4 rounded-lg backdrop-blur-lg">
+          <div className="flex justify-items-stretch gap-20">
+            <div className="w-1/2">
+              <img src={FrontImage} alt="" className="w-full rounded-lg" />
+            </div>
+            <div className=" min-h-[500px] min-w-[300px] flex flex-col">
+              <div className="flex flex-col gap-8 pt-8 ">
+                <h4 className="text-slate-100 text-4xl">Welcome Back</h4>
+                <p className="text-slate-200">
+                  Login as{" "}
+                  <span className="text-orange-400"> Admin or User</span> to
+                  continue.
+                </p>
+              </div>
+
+              <form className="flex flex-col gap-6 mt-12 ">
+                <CustomDropdown />
+                <input
+                  type="text"
+                  placeholder="Enter Username"
+                  className="fieldset"
+                />
+
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  className="fieldset"
+                />
+              </form>
+              <button className="py-3 px-10 rounded-2xl w-full mt-12 text-xl text-slate-100 bg-orange-500 hover:bg-orange-600 transition duration-300">
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
