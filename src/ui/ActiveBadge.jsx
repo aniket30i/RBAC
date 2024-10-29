f948dee9e22ebc9234d0b411149f3f0d9e5d11d2
Aniket Saha <iamaniket.saha.09@protonmail.com>
2024-10-29 18:34:55 +0530

    online counter badge

--- src/ui/ActiveBadge.jsx
+++ src/ui/ActiveBadge.jsx
@@ -3,12 +3,22 @@ import employees from "../data/employees.json";
 const active = employees.filter(
   (employee) => employee.status === "Online"
 ).length;
+const total = employees.length;
 
 console.log("size of online from actbadge comp", active);
 const ActiveBadge = () => {
   return (
-    <div className="flex justify-center mt-16 border-2 border-orange-500 text-white p-2 rounded-full w-48 h-20 ">
-      x/20
+    <div className="flex justify-center items-center md:gap-8 mt-16">
+      <div className="">
+        <p className="text-slate-100 text-3xl font-semibold">
+          Employees Online Now :{" "}
+        </p>
+      </div>
+      <div className="flex justify-center items-center border-4 border-orange-400 text-white p-2 rounded-full w-36 h-20 ">
+        <p className="text-[2rem]">
+          {active} <span className="text-orange-500">/ {total} </span>
+        </p>
+      </div>
     </div>
   );
 };
